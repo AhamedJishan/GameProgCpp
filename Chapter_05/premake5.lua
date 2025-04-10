8b1446fc629cd1a53a9e97c15a4213831e976644
Jishan Ahamed <jjahamed33@gmail.com>
2025-04-10 14:02:38 +0530

    Chapter_05: Initial Premake project setup

---
+++ Chapter_05/premake5.lua
@@ -0,0 +1,41 @@
+workspace "Chapter_05"
+    configurations {"Debug", "Release"}
+    architecture "x86_64"
+
+    project "Chapter_05"
+        kind "ConsoleApp"
+        language "C++"
+        cppdialect "C++17"
+        systemversion "latest"
+
+        targetdir "bin/"
+        objdir "bin-int/"
+
+        files {
+            "src/**.h",
+            "src/**.cpp"
+        }
+
+        includedirs {
+            "../_dependencies/include",
+            "src/Game/"
+        }
+
+        libdirs {
+            "../_dependencies/lib"
+        }
+
+        links {
+            "SDL2",
+            "SDL2main",
+            "SDL2_image",
+            "opengl32"
+        }
+
+        filter "configurations:Debug"
+            symbols "On"
+            runtime "Debug"
+
+        filter "configurations:Release"
+            symbols "Off"
+            optimize "full"
