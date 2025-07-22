dbf6eea827482bc7d4f1bc5cf024fc700630c401
Jishan Ahamed <146475810+AhamedJishan@users.noreply.github.com>
2025-07-22 23:10:23 +0530

    Chapter_10: PhysWorld::AddBox() and PhysWorld::RemoveBox() functions created

--- Chapter_10/src/Engine/PhysWorld.cpp
+++ Chapter_10/src/Engine/PhysWorld.cpp
@@ -1,6 +1,7 @@
 #include "PhysWorld.h"
 
 #include "Component/BoxComponent.h"
+#include <algorithm>
 
 namespace jLab
 {
@@ -11,10 +12,17 @@ namespace jLab
 	
 	void PhysWorld::AddBox(BoxComponent* box)
 	{
+		m_Boxes.emplace_back(box);
 	}
 	
 	void PhysWorld::RemoveBox(BoxComponent* box)
 	{
+		auto iter = std::find(m_Boxes.begin(), m_Boxes.end(), box);
+		if (iter != m_Boxes.end())
+		{
+			std::iter_swap(iter, m_Boxes.end() - 1);
+			m_Boxes.pop_back();
+		}
 	}
 	
 	bool PhysWorld::SegmentCast(const LineSegment& line, CollisionInfo& outInfo)
