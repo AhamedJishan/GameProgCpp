7f10d8ad866b8041bf0f8c728606af54cdf120d1
Jishan Ahamed <146475810+AhamedJishan@users.noreply.github.com>
2025-07-22 21:30:14 +0530

    Chapter_10: PhysWorld::SegmentCast() function created

--- Chapter_10/src/Engine/PhysWorld.cpp
+++ Chapter_10/src/Engine/PhysWorld.cpp
@@ -1,18 +1,45 @@
 #include "PhysWorld.h"
 
+#include "Component/BoxComponent.h"
+
 namespace jLab
 {
 	PhysWorld::PhysWorld(Game* game)
+		:m_Game(game)
 	{
 	}
+	
 	void PhysWorld::AddBox(BoxComponent* box)
 	{
 	}
+	
 	void PhysWorld::RemoveBox(BoxComponent* box)
 	{
 	}
+	
 	bool PhysWorld::SegmentCast(const LineSegment& line, CollisionInfo& outInfo)
 	{
-		return false;
+		bool collided = false;
+		float closestT = std::numeric_limits<float>::infinity();
+		glm::vec3 normal;
+
+		for (BoxComponent* box : m_Boxes)
+		{
+			float t;
+			if (Intersects(line, box->GetWorldBox(), t, normal))
+			{
+				if (t < closestT)
+				{
+					closestT = t;
+					outInfo.m_Point = line.PointOnSegment(t);
+					outInfo.m_Normal = normal;
+					outInfo.m_Box = box;
+					outInfo.m_Actor = box->GetOwner();
+					collided = true;
+				}
+			}
+		}
+
+		return collided;
 	}
 }
